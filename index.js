bf9d5b3af9ca8875ebbcf2fc6fdc835997978ca6
marcon29 <online@markstabler.com>
2022-03-27 11:40:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+let driver = {}
+
+// return a new driver with value for specified key
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, {[key]: value});
+}
+
+// return a new driver with value for specified key
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign(driver, {[key]: value});
+}
+
+// return new object that with exisitng driver except with specified key/value pair deleted
+function deleteFromDriverByKey(driver, key) {
+    let newDriver = {...driver};
+    delete newDriver[key];
+    return newDriver;
+}
+
+// delete (destructively) key/value pair from exisitng driver
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
